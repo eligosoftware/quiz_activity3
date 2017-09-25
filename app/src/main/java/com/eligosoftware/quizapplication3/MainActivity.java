2f0e4f23867fcaf26cd7be9cdb1239e034f5847d
Ilqar Rasulov <gragli98@gmail.com>
2017-09-25 17:23:47 +0400

    Finished chapter 2

--- app/src/main/java/com/eligosoftware/quizapplication3/MainActivity.java
+++ app/src/main/java/com/eligosoftware/quizapplication3/MainActivity.java
@@ -4,12 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private ImageButton mNextButton;
+    private ImageButton mPrevButton;
+    private TextView mQuestionTextView;
+
+    private Question[] mQuestionBank=new Question[]{
+            new Question(R.string.question_oceans,true),
+            new Question(R.string.question_mideast,false),
+            new Question(R.string.question_africa,false),
+            new Question(R.string.question_americas,true),
+            new Question(R.string.question_asia,true)
+    };
+
+    private int mCurrentIndex=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +35,60 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
         mFalseButton=(Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
 
+
+        mQuestionTextView=(TextView)findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex=(mCurrentIndex+1)%mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        updateQuestion();
+
+        mNextButton=(ImageButton)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex=(mCurrentIndex+1)%mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+        mPrevButton=(ImageButton)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex=(mQuestionBank.length+mCurrentIndex-1)%mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue=mQuestionBank[mCurrentIndex].isAnstwerTrue();
+
+        int messageResId=0;
+
+        if(userPressedTrue==answerIsTrue){
+            messageResId=R.string.correct_toast;
+        }else{
+            messageResId=R.string.incorrect_toast;
+        }
+        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateQuestion() {
+        int question=mQuestionBank[mCurrentIndex].getTextResID();
+        mQuestionTextView.setText(question);
     }
 }
